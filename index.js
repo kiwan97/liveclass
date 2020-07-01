439f843ddc9550dc384e6e6ba40369a9407c057f
kiwan97 <kwan765@naver.com>
2020-07-01 14:12:40 +0900

    init12

--- index.js
+++ index.js
@@ -6,18 +6,24 @@ const server = app.listen(3000,()=>{console.log("local 3000 is listen");});
 const io = require('socket.io').listen(server);
 console.log(io.path);
 
+
 const FPS = 15;
-const wCap = new cv.VideoCapture(0);
-wCap.set(cv.CAP_PROP_FRAME_WIDTH,300);
-wCap.set(cv.CAP_PROP_FRAME_HEIGHT,300);
+// const wCap = new cv.VideoCapture(0);
+// wCap.set(cv.CAP_PROP_FRAME_WIDTH,300);
+// wCap.set(cv.CAP_PROP_FRAME_HEIGHT,300);
 app.use("/static", express.static("static"));
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname+'/views/index.html'));
 });
 
-setInterval(() => {
-    const frame = wCap.read();
-    const image = cv.imencode('.jpg',frame).toString('base64');
-    io.emit('image', image);
-    io.emit('message', {message: "kiwan"});
-}, 1000/FPS);
+// setInterval(() => {
+//     const frame = wCap.read();
+//     const image = cv.imencode('.jpg',frame).toString('base64');
+//     io.emit('image', image);
+// }, 1000/FPS);
+
+io.on('connection', function(socket){
+    socket.on('image',function(data){
+        io.sockets.emit('image',data);
+    });
+});
--- static/main.js
+++ static/main.js
@@ -1,7 +1,28 @@
 const imageElm = document.getElementById('image2');
 const socket = io.connect('http://192.168.0.7:3000');
-console.log(socket);
+const webcamElement = document.getElementById('webcam');
+const canvasElement = document.getElementById('canvas');
+const snapSoundElement = document.getElementById('snapSound');
+const webcam = new Webcam(webcamElement, 'user', canvasElement, null);
+const btn1 = document.getElementById('btn1');
+
+btn1.addEventListener('click',function(){
+    const snapfun = function(){
+        var picture = webcam.snap();
+        socket.emit('image', picture);
+    }   
+    setInterval(snapfun,100);
+});
+
 socket.on('image',(data) => {
-    imageElm.src = `data:image/jpeg;base64,${data}`;
-    
+    imageElm.src = data;
+    console.log("data is ",data);
+});
+
+webcam.start()
+.then(result =>{
+    console.log("webcam started");
+})
+.catch(err => {
+    console.log(err);
 });
