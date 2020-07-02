8ec6aea9ba4ee50c61add2d6ba2993f5fad40677
kiwan97 <kwan765@naver.com>
2020-07-02 22:28:36 +0900

    20200702

--- index.js
+++ index.js
@@ -3,26 +3,23 @@ const express = require('express');
 const app = express();
 const server = app.listen(3000,()=>{console.log("local 3000 is listen");});
 const io = require('socket.io').listen(server);
+let userlist = [];
 console.log(io.path);
 
-
-const FPS = 15;
-// const wCap = new cv.VideoCapture(0);
-// wCap.set(cv.CAP_PROP_FRAME_WIDTH,300);
-// wCap.set(cv.CAP_PROP_FRAME_HEIGHT,300);
 app.use("/static", express.static("static"));
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname+'/views/index.html'));
 });
 
-// setInterval(() => {
-//     const frame = wCap.read();
-//     const image = cv.imencode('.jpg',frame).toString('base64');
-//     io.emit('image', image);
-// }, 1000/FPS);
-
 io.on('connection', function(socket){
     socket.on('image',function(data){
         io.sockets.emit('image',data);
+    }); 
+    socket.on('newclient',function(data){
+        userlist.push(data);
+        io.sockets.emit('newclient',data);
+    });
+    socket.on('newbie',function(data){
+        io.to(data).emit('userlist_init', userlist);
     });
 });
--- static/main.js
+++ static/main.js
@@ -1,24 +1,64 @@
-const imageElm = document.getElementById('image2');
+var imageElm = document.getElementById('image2');
 const socket = io.connect('http://192.168.0.7:3000');
 const webcamElement = document.getElementById('webcam');
 const canvasElement = document.getElementById('canvas');
 const snapSoundElement = document.getElementById('snapSound');
 const webcam = new Webcam(webcamElement, 'user', canvasElement, null);
 const btn1 = document.getElementById('btn1');
+const imgcontainer = document.getElementById('image_container');
+let userlist = [];
 
 btn1.addEventListener('click',function(){
+    const newimg = document.createElement('img');
+    newimg.id = 'image#'+socket.id;
+    userlist.push(socket.id);
+    imgcontainer.appendChild(newimg);
+    socket.emit('newclient',socket.id);
     const snapfun = function(){
         var picture = webcam.snap();
-        socket.emit('image', picture);
+        socket.emit('image', {picture:picture, id:socket.id});
     }   
-    setInterval(snapfun,100);
+    setInterval(snapfun,200);
+
+    const sendUserid = function(){
+        socket.emit('newclient',socket.id);
+    }
+    setInterval(sendUserid,1000);
+
+    btn1.style="display: none;";
 });
 
 socket.on('image',(data) => {
-    imageElm.src = data;
-    console.log("data is ",data);
+    var newid = data.id;
+    const newimg = document.getElementById('image#'+newid);
+    newimg.src = data.picture;
 });
-
+socket.on('newclient',(data) => {
+    var check = false;
+    for(var i = 0; i<userlist.length;i++){
+        if(userlist[i] == data){
+            check = true;
+            break;
+        }
+    }
+    if(check)
+        return;
+    console.log('newclient'+data);
+    var newid = data;
+    const newimg = document.createElement('img');
+    newimg.id = 'image#'+newid;
+    imgcontainer.appendChild(newimg);
+    userlist.push(data);
+});
+socket.on('userlist_init',(data) => {
+    userlist = data;
+    for(var i = 0;userlist.length;i++){
+        var newid = userlist[i];
+        const newimg = document.createElement('img');
+        newimg.id = 'image#'+newid;
+        imgcontainer.appendChild(newimg);
+    }
+});
 webcam.start()
 .then(result =>{
     console.log("webcam started");
@@ -26,3 +66,9 @@ webcam.start()
 .catch(err => {
     console.log(err);
 });
+
+function init(){
+    socket.emit('newbie',socket.id);
+}
+
+init();
